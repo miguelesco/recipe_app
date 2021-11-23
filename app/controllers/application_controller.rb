6daaadf792d8b31f7f0ad3fb7ee883693d87e37d
miguelesco <miuelangel1999@gmail.com>
2021-11-23 15:51:38 -0400

    register and loggind working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_in) { |u| u.permit(:name, :password) }
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :password) }
+
+    # devise_parameter_sanitizer.permit(:account_update) do |u|
+    #   u.permit(:name, :email, :bio, :photo, :password, :current_password)
+    # end
+  end
 end
