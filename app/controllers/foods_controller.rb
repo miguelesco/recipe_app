ae75dc3d63edf4e08894a1fa24611317b04703c6
miguelesco <miuelangel1999@gmail.com>
2021-11-24 15:57:22 -0400

    add new food working

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -3,7 +3,27 @@ class FoodsController < ApplicationController
     @foods = Food.all
   end
 
-  def create 
+  def new
+    @food = Food.new
+  end
+
+  def create
+    new_food = Food.create(name: food_params[:name], measurement_unit: food_params[:measurement_unit], price: food_params[:price], user_id: current_user.id)
+    respond_to do |format|
+      format.html do
+        if new_food.save
+          flash[:notice] = 'Food successfully created'
+          redirect_to "/foods"
+        else
+          flash[:notice] = 'It was not possible to save the Food'
+          redirect_to '/foods/new'
+        end
+      end
+    end
+  end
+
+  def food_params
+    params.require(:food).permit(:name, :measurement_unit, :price)
   end
 
   def show
