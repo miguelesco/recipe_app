a8e6a140e6c4d31cc2be07b3def044ca696ad56b
miguelesco <miuelangel1999@gmail.com>
2021-11-24 17:48:04 -0400

    cancan working and auth to delete food

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,15 +4,20 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    # Define abilities for the passed in user here. For example:
-    #
-    #   user ||= User.new # guest user (not logged in)
-    #   if user.admin?
-    #     can :manage, :all
-    #   else
-    #     can :read, :all
-    #   end
-    #
+    # user ||= User.new
+
+    can :read, User
+    can :read, Food
+
+    return unless user.present?
+
+    can :manage, Food, user_id: user.id
+
+    can :create, Food
+    
+    return unless user.role == 'admin'
+
+    can :manage, :all
     # The first argument to `can` is the action you are giving the user
     # permission to do.
     # If you pass :manage it will apply to every action. Other common actions
