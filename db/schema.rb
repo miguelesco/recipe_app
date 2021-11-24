c53b2b868b03d92d47923082121a55fc9d4176e1
miguelesco <miuelangel1999@gmail.com>
2021-11-24 17:09:04 -0400

    add role to user table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_23_192856) do
+ActiveRecord::Schema.define(version: 2021_11_24_210718) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -73,6 +73,7 @@ ActiveRecord::Schema.define(version: 2021_11_23_192856) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "role"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
