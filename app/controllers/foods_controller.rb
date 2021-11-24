725eedef437cd76a5e7f0b2cdd420ce5de8ba833
miguelesco <miuelangel1999@gmail.com>
2021-11-24 16:41:54 -0400

    rubocop fixx

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,5 +1,5 @@
 class FoodsController < ApplicationController
-  def index 
+  def index
     @foods = Food.all
   end
 
@@ -8,12 +8,13 @@ class FoodsController < ApplicationController
   end
 
   def create
-    new_food = Food.create(name: food_params[:name], measurement_unit: food_params[:measurement_unit], price: food_params[:price], user_id: current_user.id)
+    new_food = Food.create(name: food_params[:name], measurement_unit: food_params[:measurement_unit],
+                           price: food_params[:price], user_id: current_user.id)
     respond_to do |format|
       format.html do
         if new_food.save
           flash[:notice] = 'Food successfully created'
-          redirect_to "/foods"
+          redirect_to '/foods'
         else
           flash[:notice] = 'It was not possible to save the Food'
           redirect_to '/foods/new'
@@ -28,6 +29,6 @@ class FoodsController < ApplicationController
 
   def destroy
     Food.find(params[:id]).destroy
-    redirect_to "/foods"
+    redirect_to '/foods'
   end
 end
